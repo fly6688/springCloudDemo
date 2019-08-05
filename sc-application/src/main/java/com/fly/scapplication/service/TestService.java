30e0a0f3b21a08afb8c9cde821bb24039bcbee1a
xuyunfei <15999563236@163.com>
2019-08-05 17:35:43 +0800

    搭建

---
+++ sc-application/src/main/java/com/fly/scapplication/service/TestService.java
@@ -0,0 +1,11 @@
+package com.fly.scapplication.service;
+
+/**
+* @Author:         xuyunfei
+* @CreateDate:     2019/8/5 0005 17:16
+* @Description:    java类作用描述
+*/
+public interface TestService {
+
+    String testProvider(String name);
+}
---
+++ sc-pricing/src/main/java/com/fly/scpricing/ScPricingApplication.java
@@ -0,0 +1,15 @@
+package com.fly.scpricing;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+
+@SpringBootApplication
+@EnableDiscoveryClient
+public class ScPricingApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ScPricingApplication.class, args);
+    }
+
+}
