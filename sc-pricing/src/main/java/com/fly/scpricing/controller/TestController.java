40032c1080baf6f6ed6d8dc0736e0eab9e91c3f8
xuyunfei <15999563236@163.com>
2019-08-21 09:07:46 +0800

    监控

--- sc-pricing/src/main/java/com/fly/scpricing/controller/TestController.java
+++ sc-pricing/src/main/java/com/fly/scpricing/controller/TestController.java
@@ -1,6 +1,7 @@
 package com.fly.scpricing.controller;
 
 import com.fly.scpricing.service.TestService;
+import com.netflix.hystrix.contrib.javanica.annotation.HystrixCommand;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -14,7 +15,13 @@ public class TestController {
     private TestService testService;
 
     @RequestMapping("/hi")
+    @HystrixCommand(fallbackMethod = "hi")
     public String testHi(@RequestParam(name = "clientName",required = true) String clientName) {
         return testService.testProvider(clientName);
+    }
+
+    @HystrixCommand(fallbackMethod = "hi")
+    public String hi(String name) {
+        return "hi " + name + " , server is down";
     }
 }
